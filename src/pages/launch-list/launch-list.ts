299acfebe2e04b0715f2c0dfb759342ca8996a0f
Coignard Hervé <hobito@hotmail.fr>
2018-06-10 00:27:47 +0200

    Fin devnotification

--- src/pages/launch-list/launch-list.ts
+++ src/pages/launch-list/launch-list.ts
@@ -1,22 +1,36 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, NavParams, Platform } from "ionic-angular";
 import { SpacexApiProvider } from "../../providers/spacex-api/spacex-api";
 import { Launch } from "../../Models/Launch";
 import { LaunchDetailsPage } from "../launch-details/launch-details";
+import { LocalNotifications } from "@ionic-native/local-notifications";
 @IonicPage()
 @Component({
   selector: "page-launch-list",
   templateUrl: "launch-list.html"
 })
 export class LaunchListPage {
-  launches: Launch[];
+  launches: Launch[]
   constructor(
     public navCtrl: NavController,
     private navParams: NavParams,
-    private spacexAPI: SpacexApiProvider
+    private spacexAPI: SpacexApiProvider,
+    public platform: Platform,
+    public localNotifications: LocalNotifications
   ) {
+    
     this.spacexAPI.getAllLaunches().subscribe(data => {
       this.launches = data;
+    });
+    this.spacexAPI.getAllFuturLaunches().subscribe(data => {
+      data.forEach(futurLaunch => {
+        this.localNotifications.schedule({
+          id: futurLaunch.flight_number,
+          text: futurLaunch.mission_name + " Mission starting now !",
+          trigger: { at: new Date(futurLaunch.launch_date_local) },
+
+        });
+      });
     });
   }
 
