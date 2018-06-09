1c658462571569cf6e37f212b7dc332f0144ccec
Moncif <moncifloukili@gmail.com>
2018-06-09 21:52:38 +0200

    added rocket list and unit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
 import { MyApp } from "./app.component";
 
-import { AboutPage } from "../pages/about/about";
-import { ContactPage } from "../pages/contact/contact";
 import { HomePage } from "../pages/home/home";
 import { TabsPage } from "../pages/tabs/tabs";
 
@@ -15,29 +13,30 @@ import { HttpClientModule } from "@angular/common/http";
 import { LaunchListPage } from "../pages/launch-list/launch-list";
 import { LaunchDetailsPage } from "../pages/launch-details/launch-details";
 import { CompanyInfoPage } from "../pages/company-info/company-info";
-
+import { RocketsPage } from "../pages/rockets/rockets";
+import { RocketDetailPage } from "../pages/rocket-detail/rocket-detail";
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    RocketsPage,
     HomePage,
     TabsPage,
     LaunchListPage,
     LaunchDetailsPage,
+    RocketDetailPage,
     CompanyInfoPage
   ],
   imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    RocketsPage,
     HomePage,
     TabsPage,
     LaunchListPage,
     LaunchDetailsPage,
+    RocketDetailPage,
     CompanyInfoPage
   ],
   providers: [
--- src/pages/launch-details/launch-details.ts
+++ src/pages/launch-details/launch-details.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Launch } from "../../Models/Launch"
-
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { Launch } from "../../Models/Launch";
 
 /**
  * Generated class for the LaunchDetailsPage page.
@@ -12,27 +11,26 @@ import { Launch } from "../../Models/Launch"
 
 @IonicPage()
 @Component({
-  selector: 'page-launch-details',
-  templateUrl: 'launch-details.html',
+  selector: "page-launch-details",
+  templateUrl: "launch-details.html"
 })
 export class LaunchDetailsPage {
+  launch: Launch;
 
-  launch: Launch
-  
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    this.launch = this.navParams.data
-    console.log('ionViewDidLoad LaunchDetailsPage');
+    this.launch = this.navParams.data;
+    console.log("ionViewDidLoad LaunchDetailsPage");
   }
 
   goToExternalUri(uri: string) {
-    if(uri == "article") {
-      window.open(this.launch.links.article_link, '_system')
-    }else if(uri == "wiki") {
-      window.open(this.launch.links.wikipedia, '_system')
-    }else {
-      window.open(this.launch.links.video_link, '_system')
+    if (uri == "article") {
+      window.open(this.launch.links.article_link, "_system");
+    } else if (uri == "wiki") {
+      window.open(this.launch.links.wikipedia, "_system");
+    } else {
+      window.open(this.launch.links.video_link, "_system");
     }
   }
 }
